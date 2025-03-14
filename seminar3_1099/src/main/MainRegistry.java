347ab757c3a50a3ba71d544b5c717cc63502ab71
Andrei Stanciu <cris_andi2003@yahoo.ro>
2025-03-14 18:19:19 +0000

    Seminar 3

---
+++ seminar3_1099/src/main/MainRegistry.java
@@ -0,0 +1,15 @@
+package main;
+
+import anunturi.AgentieImobiliara;
+import anunturi.Anunt;
+
+public class MainRegistry {
+    public static void main(String[] args) {
+        AgentieImobiliara agentie = new AgentieImobiliara("Imobiliare.ro",120000);
+        Anunt anunt = agentie.getAnunt("Piata Romana nr. 6","Popescu",3);
+        Anunt anunt2 = agentie.getAnunt("Piata Universitatii nr. 10","Ionescu",3);
+        Anunt anunt3 = agentie.getAnunt("Piata Romana nr. 6","Vasilescu",3);
+
+        System.out.println(anunt3);
+    }
+}
